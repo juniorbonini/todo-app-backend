75b3609e6496889e4dde3584a86965e0cf687180
juniorbonini <juniorboninia7x@gmail.com>
2026-03-26 14:36:33 -0300

    feat: configura conexão com MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+
+const URI =
+  'mongodb+srv://todo-app:todoappapplication@todoapp.yle10an.mongodb.net/';
+
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    MongooseModule.forRoot(URI, {
+      onConnectionCreate: (connection) => {
+        console.log('🚀 Servidor conetado com MongoDB com sucesso!');
+        return connection;
+      },
+    }),
+    UserModule,
+  ],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
