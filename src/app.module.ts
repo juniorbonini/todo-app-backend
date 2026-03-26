9b693a0846c6d929f2fffdfacb9321224977080d
juniorbonini <juniorboninia7x@gmail.com>
2026-03-26 14:41:19 -0300

    feat: cria módulo e schema de usuário

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { UserModule } from './user/user.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
 
 const URI =
   'mongodb+srv://todo-app:todoappapplication@todoapp.yle10an.mongodb.net/';
@@ -18,7 +16,7 @@ const URI =
     }),
     UserModule,
   ],
-  controllers: [UserController],
-  providers: [UserService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
