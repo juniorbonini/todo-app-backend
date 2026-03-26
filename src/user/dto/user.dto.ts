429f2414d7f6c438d6fb3f72a4487f7328939319
juniorbonini <juniorboninia7x@gmail.com>
2026-03-26 18:18:30 -0300

    feat: cria UserResponseDTO com mapeamento de usuário

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,4 +1,12 @@
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { UserDocument } from '../schemas/user.schema';
 
 export class CreateUserDTO {
   @IsNotEmpty({ message: 'O nome é obrigatório' })
@@ -15,4 +23,25 @@ export class UserResponseDTO {
   id: string;
   name: string;
   email: string;
+
+  constructor(user: UserDocument) {
+    ((this.id = user._id.toString()),
+      (this.name = user.name),
+      (this.email = user.email));
+  }
+}
+
+export class UpdateUserDTO {
+  @IsOptional()
+  @IsString({ message: 'O nome deve ser um texto' })
+  name?: string;
+
+  @IsOptional()
+  @IsEmail({}, { message: 'Email inválido' })
+  email?: string;
+
+  @IsOptional()
+  @IsString({ message: 'A senha deve ser um texto' })
+  @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres' })
+  password?: string;
 }
