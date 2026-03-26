4c747852c3a1d87a2eee1b62a27e9562e56b5b74
juniorbonini <juniorboninia7x@gmail.com>
2026-03-26 14:45:57 -0300

    feat(user): cria estrutura inicial do módulo de usuário

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './controller/user.controller';
+import { UserService } from './service/user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, userSchema } from './schemas/user.schema';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: userSchema }]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
