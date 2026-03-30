45261093a751d09fae97caabc2003157c91b72ca
juniorbonini <juniorboninia7x@gmail.com>
2026-03-30 16:56:57 -0300

    feat: criar RegisterDTO - criado DTO com campos básicos para registro de usuário

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,27 @@
+import { IsNotEmpty, IsEmail, IsDate, IsNumber } from 'class-validator';
+
+export class RegisterDTO {
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
+  name: string;
+
+  @IsNotEmpty({ message: 'O E-mail é obrigatório' })
+  @IsEmail({}, { message: 'E-mail inválido' })
+  email: string;
+
+  @IsNotEmpty({ message: 'A senha é obrigatória' })
+  password: string;
+
+  @IsNotEmpty({ message: 'A confirmação de senha é obrigatória' })
+  confirmPassword: string;
+
+  @IsNotEmpty({ message: 'A data de nascimento é obrigatória' })
+  @IsDate({ message: 'Data de nascimento inválida' })
+  birthDate: Date;
+
+  @IsNotEmpty({ message: 'O gênero é obrigatório' })
+  gender: string;
+
+  @IsNotEmpty({ message: 'A idade é obrigatória' })
+  @IsNumber({}, { message: 'Idade inválida' })
+  age: number;
+}
