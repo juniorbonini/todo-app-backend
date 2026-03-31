d0c5b6c1e1baa8a35f96f7f94ace09e539b96daa
juniorbonini <juniorboninia7x@gmail.com>
2026-03-31 17:08:47 -0300

    feat(task): configura MongooseModule e registra TaskSchema no módulo de tarefas

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,9 +1,14 @@
+import { TaskController } from '@/task/controller/task.controller';
+import { TaskService } from '@/task/service/task.service';
 import { Module } from '@nestjs/common';
-import { TaskService } from './service/task.service';
-import { TaskController } from './controller/task.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Task, TaskSchema } from './schema/task.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+  ],
   providers: [TaskService],
-  controllers: [TaskController]
+  controllers: [TaskController],
 })
 export class TaskModule {}
