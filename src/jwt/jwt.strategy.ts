0613afc34e87f752019d9f41539ec47425fb21e9
juniorbonini <juniorboninia7x@gmail.com>
2026-03-31 14:32:42 -0300

    feat(auth): cria JwtStrategy para extração e validação do token no header

---
+++ src/jwt/jwt.strategy.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+import { Injectable } from '@nestjs/common';
+import { JWT_SECRET } from '@/auth/auth.module';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: JWT_SECRET,
+    });
+  }
+  async validate(payload: any) {
+    console.log('Payload JWT: ', payload);
+    return { userId: payload.sub, email: payload.email };
+  }
+}
