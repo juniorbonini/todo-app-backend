52b3ccfb3bbb3b8cab5304c403fbb7c0df318652
juniorbonini <juniorboninia7x@gmail.com>
2026-04-13 18:39:32 -0300

    fix(user): persist register profile fields

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -11,6 +11,15 @@ export class User {
 
   @Prop({ required: true })
   password: string;
+
+  @Prop({ required: true })
+  birthDate: Date;
+
+  @Prop({ required: true })
+  gender: string;
+
+  @Prop({ required: true })
+  age: number;
 }
 
 export type UserDocument = User & Document;
