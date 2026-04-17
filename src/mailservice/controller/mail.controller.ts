d79f45e2740813f5f05543397e5ce41ec857c420
juniorbonini <juniorboninia7x@gmail.com>
2026-04-17 15:32:39 -0300

    refactor(mailservice): simplifica controller de e-mail

--- src/mailservice/controller/mail.controller.ts
+++ src/mailservice/controller/mail.controller.ts
@@ -1,48 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
-import { CreateMailserviceDto } from '../dto/create-mailservice.dto';
-import { UpdateMailserviceDto } from '../dto/update-mailservice.dto';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { VerifyCodeDTO } from '@/auth/dto/verify.code.dto';
 import { MailService } from '../service/mail.service';
 
 @Controller('mailservice')
 export class MailserviceController {
   constructor(private readonly mailService: MailService) {}
 
-  @Post()
-  create(@Body() createMailserviceDto: CreateMailserviceDto) {
-    return this.mailService.create(createMailserviceDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.mailService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.mailService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateMailserviceDto: UpdateMailserviceDto,
-  ) {
-    return this.mailService.update(+id, updateMailserviceDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.mailService.remove(+id);
+  @Post('verification-code')
+  verificationCode(@Body() dto: VerifyCodeDTO) {
+    return this.mailService.sendVerificationCode(dto.email, dto.code);
   }
 }
