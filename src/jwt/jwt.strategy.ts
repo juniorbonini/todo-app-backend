ca3365be8dd4988936a07eb66e740a73f2475449
juniorbonini <juniorboninia7x@gmail.com>
2026-04-17 17:44:48 -0300

    fix(jwt): ajusta estratégia de autenticação

--- src/jwt/jwt.strategy.ts
+++ src/jwt/jwt.strategy.ts
@@ -1,23 +1,27 @@
-/* eslint-disable @typescript-eslint/require-await */
-
+import { ActiveUser } from '@/interfaces/active.user';
+import { JwtPayload } from '@/interfaces/jwt.payload';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
-import { JWT_SECRET } from '@/auth/auth.module';
-import { ActiveUser } from '@/interfaces/active.user';
-import { JwtPayload } from '@/interfaces/jwt.payload';
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(config: ConfigService) {
+    const secret = config.get<string>('JWT_SECRET');
+
+    if (!secret) {
+      throw new Error('JWT_SECRET must be defined');
+    }
+
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: JWT_SECRET,
+      secretOrKey: secret,
     });
   }
-  async validate(payload: JwtPayload): Promise<ActiveUser> {
+
+  validate(payload: JwtPayload): ActiveUser {
     return { id: payload.sub, email: payload.email };
   }
 }
